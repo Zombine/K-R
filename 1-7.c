941164d27f2ef39dd32e07534d1c4cb45d8726e6
Zombine <dxm.dxo@gmail.com>
2011-07-13 13:31:32 -0700

    Prints the value of EOF.

---
+++ 1-7.c
@@ -0,0 +1,13 @@
+//Write a program to print the value of EOF.
+//Prints EOF as -1
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  printf("%d\n", EOF); 
+ // system("PAUSE");	<---------NOPE.jpg
+ printf("Press [ENTER] to exit.");
+ getchar();
+  return 0;
+}
